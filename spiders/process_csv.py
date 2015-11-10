71693576a5151acea38c59d07eaa09efcd87c41d
zjmmjzzjm <zjmmjzzjm@126.com>
2015-11-10 17:55:15 +0800

    update

--- spiders/process_csv.py
+++ spiders/process_csv.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 	import sys
 	csvdir = sys.argv[1]
 	container = set()
-	outf = open(sys.argv[2], "a")
+	outf = open(sys.argv[2], "w")
 	writer = csv.writer(outf)
 	dupnum = 0
 	totalnum = 0
@@ -18,27 +18,31 @@ if __name__ == "__main__":
 			fullpath = root + "/" + f
 			fh = open(fullpath, "r")
 			reader = csv.reader(fh)
-			for infos in reader:
-				try:
-					totalnum += 1
-					hashinfo = infos[0]
-					content = infos[1]
-					mag = infos[2]
-					if(hashinfo in container):
-						dupnum += 1
-						continue
-					container.add(hashinfo)
-					writer.writerow([hashinfo, content, mag])
-
-					#print mag 
-				except Exception,e:
-					print e
-					print "fullpath: ", fullpath
-					print infos
-					#sys.exit(0)
-
 			print fullpath
-			print "total num ", totalnum, " dupnum ", dupnum, " dup rate ", float(dupnum)/totalnum, " effective num ", totalnum - dupnum
+			try:
+				for infos in reader:
+					try:
+						totalnum += 1
+						hashinfo = infos[0]
+						content = infos[1]
+						mag = infos[2]
+						if(hashinfo in container):
+							dupnum += 1
+							continue
+						container.add(hashinfo)
+						writer.writerow([hashinfo, content, mag])
+
+						#print mag 
+					except Exception,e:
+						print e
+						print "fullpath: ", fullpath
+						print infos
+						#sys.exit(0)
+				print "total num ", totalnum, " dupnum ", dupnum, " dup rate ", float(dupnum)/totalnum, " effective num ", totalnum - dupnum
+			except Exception, e:
+				print e
+
+
 			fh.close()
 
 	outf.close()
