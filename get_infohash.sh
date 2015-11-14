a51e34d05f3f6f11b5b07d000e633602de4cc358
zjm <zjmmjzzjm@126.com>
2015-11-14 11:50:46 +0800

    upate

---
+++ get_infohash.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+dt=`date +%Y%m%d`
+cd download
+mkdir $dt
+cd $dt
+cur=`date +%s`
+
+
+cmd="sftp root@128.199.121.74 <<H\ncd dht-work/dht-test/infohash/"
+echo $cur
+for((i=1; i<8; i++))
+do
+	((d=$cur-$i*86400))
+
+	dd=`date +%Y%m%d -d @$d` 
+	cmd="$cmd\n get ${dd}_test"
+done
+cmd="$cmd \nH"
+echo -e $cmd > tmp.sh
+chmod +x tmp.sh
+./tmp.sh
+
+
