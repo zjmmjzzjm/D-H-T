9500daff682f4af8d7ddb89f27a4a8c8aa08bacb
zjm <zjmmjzzjm@126.com>
2015-12-11 22:57:57 +0800

    update

--- get_infohash.sh
+++ get_infohash.sh
@@ -1,14 +1,23 @@
 #/bin/bash
+
+if [ $1"x" != "x" ];
+then
+	echo "set days"
+	((days=$1+1))
+else
+	days=8
+fi
 dt=`date +%Y%m%d`
 cd download
 mkdir $dt
 cd $dt
 cur=`date +%s`
 
+echo $days
 
 cmd="sftp root@128.199.121.74 <<H\ncd dht-work/dht-test/infohash/"
 echo $cur
-for((i=1; i<8; i++))
+for((i=1; i<$days; i++))
 do
 	((d=$cur-$i*86400))
 
