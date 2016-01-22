f9f0e5b896c86d8fdc3f6a84b11c455c33d30824
zjm <zjmmjzzjm@126.com>
2016-01-23 02:32:48 +0600

    update

--- auto_index.sh
+++ auto_index.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
 cd $basepath
-python csv_to_msyql.py $1
+python csv_to_mysql.py $1
 /usr/local/coreseek/csft/bin/indexer delta --rotate
-/usr/local/coreseek/csft/bin/indexer –merge main delta --rotate
+/usr/local/coreseek/csft/bin/indexer --merge main delta --rotate
+python mysql.py -u
 
