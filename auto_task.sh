05898d154fd1133452c5877207c2aaf059882393
zjm <zjmmjzzjm@126.com>
2016-01-23 04:50:52 +0800

    update

--- auto_task.sh
+++ auto_task.sh
@@ -4,8 +4,8 @@ cd $basepath
 yesterday=`date -d 'yesterday' +%Y%m%d`
 file=${yesterday}_test 
 scp root@128.199.121.74:/root/dht-work/dht-test/infohash/$file download/$file
-head  -1000 download/$file > download/test.txt
-mv download/test.txt download/$file
+#head  -1000 download/$file > download/test.txt
+#mv download/test.txt download/$file
 python download_torrent.py download/$file
 python parse_torrent.py torrents/$file/
 csv_file=`echo torrents/${file}_magnet.csv | sed 's#/#_#g'`
