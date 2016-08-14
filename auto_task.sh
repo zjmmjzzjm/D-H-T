6b1c51c1a467c475350618dbff66ffeeebb6feaa
zjm <zjmmjzzjm@126.com>
2016-08-14 13:45:24 +0800

    update

--- auto_task.sh
+++ auto_task.sh
@@ -2,11 +2,13 @@
 basepath=$(cd `dirname $0`; pwd)
 cd $basepath
 yesterday=`date -d 'yesterday' +%Y%m%d`
-file=${yesterday}_test 
-scp root@128.199.121.74:/root/dht-work/dht-test/infohash/$file download/$file
+#file=${yesterday}_test 
+#scp root@128.199.121.74:/root/dht-work/dht-test/infohash/$file download/$file
 #head  -1000 download/$file > download/test.txt
 #mv download/test.txt download/$file
-python download_torrent.py download/$file
+#python download_torrent.py download/$file
+file=${yesterday}_meta
+#scp -r root@128.199.121.74:/root/dht-work/dht-test/metadata/$file torrents/
 python parse_torrent.py torrents/$file/
 csv_file=`echo torrents/${file}_magnet.csv | sed 's#/#_#g'`
 scp $csv_file root@www.btmilk.com:/root/Workspace/data/
